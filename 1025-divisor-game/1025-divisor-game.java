a35f44881741d8130ef003dbff0e63c7cbdd3c76
Rohan Sharma <iamrohan29@gmail.com>
2022-06-21 23:54:24 +0530

    initial commit

--- 1025-divisor-game/1025-divisor-game.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public boolean divisorGame(int n) {
-        return ((n & 1) == 0);
-    }
-    
-}
--- 136-single-number/136-single-number.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-        int xor = 0;
-        for(int e : nums){
-            xor = xor ^ e;
-        }
-        return xor;
-    }
-}
